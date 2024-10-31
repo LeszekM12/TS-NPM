1454237d1407c683c0b5de3eb743a52a111f7ca2
Leszek Mikrut <leszekmikrut60@gmail.com>
2024-10-31 14:14:38 +0200

    function debugger

---
+++ .idea/ts.tsx
@@ -0,0 +1,11 @@
+
+    export function f() {
+        debugger;
+        switch (process.platform) {
+            case "win32":
+            const x = () => {
+                  typeof <textarea name="" id="" cols="30" rows="10"></textarea>
+                }
+        };
+
+    };
